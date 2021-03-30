5ab5a6df4966b86006acb8e8d7d44ef7c4d854f6
leonardovida <lleonardovida@gmail.com>
2021-03-30 17:36:56 +0200

    FIrst commit projects

---
+++ CONTRIBUTING.md
@@ -0,0 +1,17 @@
+# Contributing
+
+Your contributions are always welcome!
+
+## Guideline
+
+* Add one link per Pull Request.
+    * Make sure the PR title is in the format of `Add project-name`.
+    * Write down the reason why the project is awesome.
+* For each project, please add the following:
+  * Add the main link of the project.
+  * Add a concise and **short** description of the project.
+  * Add the category to which the project belongs, if the category is not present, add a new one. Please try not to add a category if there is already one that might fit.
+* Search previous Pull Requests or Issues before making a new one, as yours may be a duplicate.
+* Don't mention `Citizen Science` in the description as it's implied.
+* Check your spelling and grammar.
+* Remove any trailing whitespace.
---
+++ parse.py
@@ -0,0 +1,54 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+import pandas as pd
+
+# Path data
+XLSX = "data/citizen-science-projects.xlsx"
+CSV = "data/citizen-science-projects.csv"
+final_README = str
+
+# Convert from xsls to csv in pandas
+excel_file = pd.read_excel(XLSX)
+excel_file.to_csv(CSV)
+
+# Read csv
+csv = pd.read_csv(CSV)
+
+categories = pd.unique(csv["Category"])
+categories.sort()
+
+with open('README.md', 'r') as read_me_file:
+    read_me = read_me_file.read()
+    main_text = ''.join(read_me.split('- - -')[0])
+    toc = "\n\n- [Awesome Citizen Science Projects](#awesome-citizen-science-projects)\n"
+
+    # Add catogories to table of content
+    for cat in range(len(categories)):
+        toc = toc + \
+            f"  - [{categories[cat]}](#{categories[cat]})" + "\n"
+
+# Add first part and toc to README
+final_README = main_text + "- - -" + toc + "\n- - -"
+
+# Add individual categories to README
+list_blocks = ""
+for cat in range(len(categories)):
+    block = f"\n\n## {categories[cat]}\n"
+    filtered = csv[csv["Category"] == categories[cat]]
+    list_items = ""
+    for i, r in filtered.iterrows():
+        try:
+            start_date = int(r['Start Date'])
+        except:
+            start_date = "NA"
+        project = f"* [{r['Name']}]({r['Main Source']}) - {r['Description']} (`{start_date} - {str(r['End Date'])}`) \n"
+        list_items = list_items + project
+    list_blocks = list_blocks + block + list_items
+
+# Add to README.md
+final_README = final_README + list_blocks
+
+# Save to new README.md
+with open('README.md', 'w+') as sorted_file:
+    sorted_file.write(final_README)
